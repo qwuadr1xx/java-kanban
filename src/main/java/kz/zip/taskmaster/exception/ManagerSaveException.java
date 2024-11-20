46f040c4461f46ae5812845ef2c1902cc8b1786a
qwuadr1xx <qwuadrixx@gmail.com>
2024-11-20 22:39:38 +0500

    bugfix/CheckStyle in all files

--- src/main/java/kz/zip/taskmaster/exception/ManagerSaveException.java
+++ src/main/java/kz/zip/taskmaster/exception/ManagerSaveException.java
@@ -1,7 +1,7 @@
 package kz.zip.taskmaster.exception;
 
-public class ManagerSaveException extends Exception{
-    public ManagerSaveException(String message, Exception exception){
+public class ManagerSaveException extends Exception {
+    public ManagerSaveException(String message, Exception exception) {
         super(message, exception);
     }
 }
