a633aeb1c4ba86efe42a2d8cc8af7ed19ea72543
qwuadr1xx <qwuadrixx@gmail.com>
2024-12-08 20:25:29 +0500

    bugfix/ clean up code

--- src/main/java/kz/zip/taskmaster/utils/DateTimeUtils.java
+++ src/main/java/kz/zip/taskmaster/utils/DateTimeUtils.java
@@ -6,5 +6,7 @@ public class DateTimeUtils {
     public static final DateTimeFormatter formatterDT = DateTimeFormatter.ofPattern("HH:mm dd.MM.yyyy");
     public static final DateTimeFormatter formatterT = DateTimeFormatter.ofPattern("HH:mm");
 
-    private DateTimeUtils(){}
+    private DateTimeUtils() {
+
+    }
 }
