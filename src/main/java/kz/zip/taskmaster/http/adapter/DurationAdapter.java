3457bf5e193386bd10315b008fd21572f848145c
qwuadr1xx <qwuadrixx@gmail.com>
2024-12-19 00:59:54 +0500

    bugfix/поправить обработку null значений в DurationAdapter

--- src/main/java/kz/zip/taskmaster/http/adapter/DurationAdapter.java
+++ src/main/java/kz/zip/taskmaster/http/adapter/DurationAdapter.java
@@ -2,6 +2,7 @@ package kz.zip.taskmaster.http.adapter;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 
 import java.io.IOException;
@@ -11,11 +12,20 @@ public class DurationAdapter extends TypeAdapter<Duration> {
 
     @Override
     public void write(final JsonWriter jsonWriter, final Duration duration) throws IOException {
+        if (duration == null) {
+            jsonWriter.nullValue();
+            return;
+        }
         jsonWriter.value(duration.toMinutes());
     }
 
     @Override
     public Duration read(final JsonReader jsonReader) throws IOException {
+        if (jsonReader.peek() == JsonToken.NULL) {
+            jsonReader.nextNull();
+            return null;
+        }
+
         return Duration.ofMinutes(Integer.parseInt(jsonReader.nextString()));
     }
 }
